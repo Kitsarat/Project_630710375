aa1caa775298fb6e6fb7820a7253ed22d40093de
Arinchai <pangpondkung08112544@gmail.com>
2022-11-13 15:46:48 +0700

    hi

--- lib/screens/details/components/product_title_with_image.dart
+++ lib/screens/details/components/product_title_with_image.dart
@@ -15,58 +15,54 @@ class ProductTitleWithImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
-      child: ListView(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: 20,),
-              Text(
-                "Gaming Gear For You",
-                style: TextStyle(color: Colors.black),
-              ),
-              Text(
-                product.title,
-                style: Theme.of(context)
-                    .textTheme
-                    .headline4
-                    .copyWith(color: Colors.black, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: kDefaultPaddin),
-              Row(
-                children: <Widget>[
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(text: "Price\n"),
-                        TextSpan(
-                          text: "\฿${product.price}",
-                          style: Theme.of(context).textTheme.headline4.copyWith(
-                              color: Colors.black, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(width: kDefaultPaddin),
-                  Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Hero(
-                            tag: "${product.id}",
-                            child: Image.asset(
-                              product.image,width: 300,
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-
-                ],
-              )
-            ],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(height: 20,),
+          Text(
+            "Gaming Gear For You",
+            style: TextStyle(color: Colors.black),
           ),
+          Text(
+            product.title,
+            style: Theme.of(context)
+                .textTheme
+                .headline4
+                .copyWith(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: kDefaultPaddin),
+          Row(
+            children: <Widget>[
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(text: "Price\n"),
+                    TextSpan(
+                      text: "\฿${product.price}",
+                      style: Theme.of(context).textTheme.headline4.copyWith(
+                          color: Colors.black, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(width: kDefaultPaddin),
+              Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Hero(
+                        tag: "${product.id}",
+                        child: Image.asset(
+                          product.image,width: 300,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+            ],
+          )
         ],
       ),
     );
